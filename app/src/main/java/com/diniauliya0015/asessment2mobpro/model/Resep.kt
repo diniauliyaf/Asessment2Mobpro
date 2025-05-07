940134ff524c10883e7a47e1b0151c0e2195b05c
Dini Auliya Fauziah <diniauliyaf@gmail.com>
2025-05-08 03:19:01 +0700

    Menyiapkan data di viewmodel

---
+++ app/src/main/java/com/diniauliya0015/asessment2mobpro/model/Resep.kt
@@ -0,0 +1,8 @@
+package com.diniauliya0015.asessment2mobpro.model
+
+data class Resep(
+    val id: Long,
+    val namaResep: String,
+    val bahan: String,
+    val langkah: String,
+)
---
+++ app/src/main/java/com/diniauliya0015/asessment2mobpro/ui/screen/MainViewModel.kt
@@ -0,0 +1,39 @@
+package com.diniauliya0015.asessment2mobpro.ui.screen
+
+import com.diniauliya0015.asessment2mobpro.model.Resep
+
+class MainViewModel {
+    val data = listOf(
+        Resep(
+            1,
+            "Ikan",
+            "cabe, bawang, tomat",
+            "goreng"
+        ),Resep(
+            2,
+            "Ikan",
+            "cabe, bawang, tomat",
+            "goreng"
+        ),Resep(
+            1,
+            "Ikan",
+            "cabe, bawang, tomat",
+            "goreng"
+        ),Resep(
+            3,
+            "Ikan",
+            "cabe, bawang, tomat",
+            "goreng"
+        ),Resep(
+            4,
+            "Ikan",
+            "cabe, bawang, tomat",
+            "goreng"
+        ),Resep(
+            5,
+            "Ikan",
+            "cabe, bawang, tomat",
+            "goreng"
+        )
+    )
+}
